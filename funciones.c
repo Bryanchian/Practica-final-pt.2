836ce466d44f7506ce9c7515c07e3b6619fd5a32
Bryanchian <bryanjosue2004@gmail.com>
2025-11-17 18:50:22 -0600

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -1,12 +1,20 @@
 #include <math.h>
+#include <stdlib.h>
+#include "funciones.h"
 
-double predict(double coef[], double x[], int n) {
-    double y = coef[0];
-    for (int i = 0; i < n; i++) y += coef[i + 1] * x[i];
-    return y;
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+double normal(double mean, double stddev) {
+    double r = drand48();
+    double q = drand48();
+    double R = sqrt(-2.0 * log(r));
+    return mean + stddev * R * cos(2.0 * M_PI * q);
 }
 
-double yf(double y0, double x, double v, double theta_rad) {
-    double t = x / (v * cos(theta_rad));
-    return y0 + v * sin(theta_rad) * t - 0.5 * 9.81 * t * t;
+double yf(double y0, double x, double v, double theta) {
+    double t = x / (v * cos(theta));
+    return y0 + v * sin(theta) * t - 0.5 * 9.81 * t * t;
 }
+

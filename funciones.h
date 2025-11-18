1b72db4f929d6c9429ee11d47fdbf6f3fc00c038
Bryanchian <bryanjosue2004@gmail.com>
2025-11-17 18:50:45 -0600

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,7 +1,8 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-double predict(double coef[], double x[], int n);
-double yf(double y0, double x, double v, double theta_rad);
+double normal(double mean, double stddev);
+double yf(double y0, double x, double v, double theta);
 
 #endif
+
